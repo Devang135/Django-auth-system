fe64790b3004ab0355b4247448b0e50f34f1b2f2
Devang Nigam <devangnigam22222@gmail.com>
2022-09-27 00:35:45 +0530

    Login register now working

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from atexit import register
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 
 
 urlpatterns = [
     path('',views.home),
     path('home',views.home,name = 'home'),
-    path('register',views.register,name ='register')
+    path('register',views.register,name ='register'),
+    path('login',auth_view.LoginView.as_view(template_name = 'users/login.html'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from contextlib import redirect_stderr
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+
 
 
 # Create your views here.
@@ -7,4 +12,13 @@ def home(request):
     return render(request,'users/home.html')
 
 def register(request):
-    return render(request,'users/register.html')
+    if request.method=="POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        username = form.cleaned_data.get('username')
+        messages.success(request,f'Hi{username} Your account is created successfully')
+        return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request,'users/register.html',{'form':form})
