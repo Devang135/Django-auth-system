7cc0fd66df53c623796a6dc1e1682507a1550039
Devang Nigam <devangnigam22222@gmail.com>
2022-09-27 23:45:31 +0530

    password-reset-funtionality-added

--- loginservice/settings.py
+++ loginservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from tkinter import E
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_FILE_PATH = BASE_DIR/'emails'
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from atexit import register
-from django.urls import path
+from django.urls import path,include
 from . import views
 from django.contrib.auth import views as auth_view
 
@@ -11,6 +11,9 @@ urlpatterns = [
     path('register',views.register,name ='register'),
     path('profile',views.profile,name ='profile'),
     path('login',auth_view.LoginView.as_view(template_name = 'users/login.html'),name= 'login'),
-    path('logout',auth_view.LogoutView.as_view(template_name = 'users/logout.html'),name ='logout')
-
+    path('logout',auth_view.LogoutView.as_view(template_name = 'users/logout.html'),name ='logout'),
+    path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='passwords/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name="passwords/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_view.PasswordResetCompleteView.as_view(template_name='passwords/password_reset_complete.html'), name='password_reset_complete'),
+    path("password_reset", views.password_reset_request, name="password_reset")
 ]
